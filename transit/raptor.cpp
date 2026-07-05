061fb841096f9cd44254117ee92d4bc296037b11
B8saur <bbromblik@gmail.com>
2026-07-05 20:48:04 +0200

    Copied main ;)

--- transit/raptor.cpp
+++ transit/raptor.cpp
@@ -19,31 +19,50 @@ void printTime(seconds curTime) {
 }
 
 int main() {
+    cout << "Loading data...\n";
+    cout.flush();
     vector<stops> stopsTable = getStops();
     vector<times> timesTable = getTimes();
     vector<trips> tripsTable = getTrips();
+
     vector<routes> allRoutes = getRoutes(stopsTable, timesTable, tripsTable);
+    cout << "All data loaded\n";
+    cout.flush();
 
-    for(auto &route : allRoutes) {
-        cout << "    " << route.stopsList.size() << "\n";
-        for(auto stopId : route.stopsList) {
-            cout << stopId << " -> ";
+    cout << "Choose start, end and start time measured in seconds from midnight (86400 is a whole day)\n";
+    cout << "Stops range from " << 0 << " to " << stopsTable.size()-1 << "\n";
+    cout << "Ex. 21 37 36000\n";
+    cout.flush();
+
+    int start, end;
+    seconds startTime;
+    while(true) {
+        cout << "###:  ";
+        cout.flush();
+        cin >> start >> end >> startTime;
+        if(start < 0 || start >= stopsTable.size() || end < 0 || end >= stopsTable.size()) {
+            cout << "Chosen stops are out of range, try again\n";
+            cout.flush();
+            continue;
         }
-        cout << "\n";
+        cout << "Start time is ";
+        printTime(startTime);
+        cout << "\nCalculating...\n";
+        cout.flush();
 
-        cout << "    " << route.startTimes.size() << "\n";
-        for(auto start : route.startTimes) {
-            printTime(start);
-            cout << " ";
+        //TODO: link actual function
+        seconds endTime = startTime;
+
+        if(endTime == NEVER) {
+            cout << "Sorry, no connection here\n";
         }
-        cout << "\n";
-
-        cout << "    " << route.between.size() << "\n";
-        for(auto betweenTime : route.between) {
-            cout << " " << betweenTime;
+        else {
+            cout << "Earliest arrival found at " << endTime << " = ";
+            printTime(endTime);
+            cout << "\n";
         }
-        cout << "\n";
-
-        cout << "############\n";
+        cout << "Try again\n";
+        cout.flush();
     }
+    return 0;
 }
